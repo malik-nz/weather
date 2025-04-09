bd8cf3ec189558a7f3c39d88f1773a99deacdfa4
malkhan <malkhanmalik@gmail.com>
2025-04-09 13:03:08 +1200

    updates

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/wather/', // Set this to your GitHub repository name for production
+  base: '/weather/', // Set this to your GitHub repository name for production
   build: {
     outDir: 'dist', // Directory where the build output will go
   },
